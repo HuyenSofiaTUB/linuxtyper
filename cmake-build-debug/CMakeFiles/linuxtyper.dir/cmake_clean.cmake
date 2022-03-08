6d2eaef1ecae216194019c7efb55c9a31139fe29
nganhuyenthinguyen <thinguyen@campus.tu-berlin.de>
2022-03-08 11:36:17 +0100

    meow

---
+++ cmake-build-debug/CMakeFiles/linuxtyper.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/linuxtyper.dir/main.c.o"
+  "CMakeFiles/linuxtyper.dir/main.c.o.d"
+  "linuxtyper"
+  "linuxtyper.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/linuxtyper.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/linuxtyper.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for linuxtyper.
+# This may be replaced when dependencies are built.
