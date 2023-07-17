175930349d99ac9ff984058f4acf81cebb920b4f
huyenngn <huyenproducts@gmail.com>
2023-07-17 15:44:03 +0200

    fixes

--- install.sh
+++ install.sh
@@ -11,5 +11,4 @@ mkdir /opt/linuxtyper
 mkdir /usr/share/icons/linuxtyper
 mv ../linuxtyper.desktop /usr/share/applications
 mv linuxtyper /opt/linuxtyper
-mv ../gui.glade /opt/linuxtyper
 mv ../icon.png /usr/share/icons/linuxtyper
