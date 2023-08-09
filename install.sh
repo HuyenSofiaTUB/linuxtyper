598d60136ff717448f6d6a4a75a5f0c811bf879f
Huyen Nguyen <48179958+huyenngn@users.noreply.github.com>
2023-08-09 21:21:52 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+sudo rm -rf /opt/linuxtyper
 sudo mkdir /opt/linuxtyper
+sudo rm -rf /usr/share/icons/linuxtyper
 sudo mkdir /usr/share/icons/linuxtyper
-sudo mv ../linuxtyper.desktop /usr/share/applications
-sudo mv linuxtyper /opt/linuxtyper
-sudo mv ../icon.png /usr/share/icons/linuxtyper
+sudo mv linuxtyper.desktop /usr/share/applications
+sudo mv build/linuxtyper /opt/linuxtyper
+sudo mv icon.png /usr/share/icons/linuxtyper
